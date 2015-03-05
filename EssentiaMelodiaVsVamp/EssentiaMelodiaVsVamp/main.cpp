90bbee25e487e0f1da747cc169c4d1b67f2dafd0
nkroher <nadine.kroher@upf.edu>
2015-03-05 11:00:58 +0100

    Initial Commit
    Proposal, Xcode project setup for Melodia Vamp Vs. Essentia

---
+++ EssentiaMelodiaVsVamp/EssentiaMelodiaVsVamp/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  EssentiaMelodiaVsVamp
+//
+//  Created by Nadine Kroher on 04/03/15.
+//  Copyright (c) 2015 Nadine Kroher. All rights reserved.
+//
+
+#include <iostream>
+#include "essentia.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
