9014940a5b833a9580b79ea0abb72d082214c644
nkroher <nadine.kroher@upf.edu>
2015-03-14 16:44:53 +0100

    Ground Truth Annotations
    4 monophonic audio tracks. Manual MIDI annotation + .notes text file;
    f0 extracted with Essentia Melodia monophonic (prototype), polyphonic,
    Vamp Melodia monophonic and Yin FFT

--- MelodiaMonophonic/EssentiaMelodiaVsVamp/main.cpp
+++ MelodiaMonophonic/EssentiaMelodiaVsVamp/main.cpp
@@ -19,7 +19,7 @@ using namespace essentia::standard;
 int main(int argc, const char * argv[]) {
     
     // file path
-    string audioFilename="/Users/GinSonic/MTG/EssentiaProject/Data/audioMono/track1.wav";
+    string audioFilename="/Users/GinSonic/MTG/EssentiaProject/Data/Monophonic/wav/flute.wav";
     
     // parameters
     int sampleRate = 44100;
@@ -30,28 +30,115 @@ int main(int argc, const char * argv[]) {
     // algorithm setup
     essentia::init();
     AlgorithmFactory& factory = standard::AlgorithmFactory::instance();
+    
+    // MELODIA
     Algorithm* audio = factory.create("MonoLoader","filename", audioFilename,"sampleRate", sampleRate);
-    Algorithm* predmel = factory.create("MelodiaMonophonic", "sampleRate", sampleRate, "frameSize", frameSize, "hopSize", hopSize, "voicingTolerance", voicingTolerance);
+    Algorithm* predmelMono = factory.create("MelodiaMonophonic", "sampleRate", sampleRate, "frameSize", frameSize, "hopSize", hopSize, "voicingTolerance", voicingTolerance);
+    Algorithm* predmel = factory.create("PredominantMelody", "sampleRate", sampleRate, "frameSize", frameSize, "hopSize", hopSize, "voicingTolerance", voicingTolerance);
     
     // Algorithm I/O setup
-    vector<Real> audioVec, pitch, pitchConfidence;
+    vector<Real> audioVec, pitch, pitchConfidence, pitchMono, pitchConfidenceMono;
     audio->output("audio").set(audioVec);
     predmel->input("signal").set(audioVec);
     predmel->output("pitch").set(pitch);
     predmel->output("pitchConfidence").set(pitchConfidence);
+    predmelMono->input("signal").set(audioVec);
+    predmelMono->output("pitch").set(pitchMono);
+    predmelMono->output("pitchConfidence").set(pitchConfidenceMono);
     
     // Compute
     audio->compute();
     predmel->compute();
+    predmelMono->compute();
 
+    
+    // YIN FFT
+    Algorithm* frameCutter = factory.create("FrameCutter",
+                                            "frameSize", frameSize,
+                                            "hopSize", hopSize,
+                                            "startFromZero", false);
+    
+    Algorithm* window = factory.create("Windowing",
+                                       "type", "hann",
+                                       "zeroPadding", 0);
+    
+    Algorithm* spectrum = factory.create("Spectrum",
+                                         "size", frameSize);
+    
+    Algorithm* pitchDetect = factory.create("PitchYinFFT",
+                                            "frameSize", frameSize,
+                                            "sampleRate", sampleRate);
+    
+    // set frameCutter:
+    vector<Real> frame;
+    frameCutter->input("signal").set(audioVec);
+    frameCutter->output("frame").set(frame);
+    
+    // set windowing:
+    vector<Real> windowedframe;
+    window->input("frame").set(frame);
+    window->output("frame").set(windowedframe);
+    
+    // set spectrum:
+    vector<Real> spec;
+    spectrum->input("frame").set(windowedframe);
+    spectrum->output("spectrum").set(spec);
+    
+    // set pitch extraction:
+    Real thisPitch = 0., thisConf = 0;
+    pitchDetect->input("spectrum").set(spec);
+    pitchDetect->output("pitch").set(thisPitch);
+    pitchDetect->output("pitchConfidence").set(thisConf);
+    
+    vector<Real> pitchYin;
+    // process:
+    while (true) {
+        frameCutter->compute();
+        
+        if (!frame.size())
+            break;
+        
+        if (isSilent(frame))
+            continue;
+        
+        window->compute();
+        spectrum->compute();
+        pitchDetect->compute();
+        
+        pitchYin.push_back(thisPitch);
+    }
+
+    
     // write to file
-    string csvFilename="/Users/GinSonic/MTG/EssentiaProject/Data/audioMono/track1.csv";
+    string csvFilename="/Users/GinSonic/MTG/EssentiaProject/Data/Monophonic/f0PolyMelodiaEssentia/flute.csv";
     ofstream outFile;
     outFile.open(csvFilename);
     for (int ii=0; ii<pitch.size(); ii++){
         outFile << float(ii)*hopSize/sampleRate << ", " << pitch[ii] << endl;
     }
+    string csvFilenameMono="/Users/GinSonic/MTG/EssentiaProject/Data/Monophonic/f0MonoMelodiaEssentia/flute.csv";
+    ofstream outFileMono;
+    outFileMono.open(csvFilenameMono);
+    for (int ii=0; ii<pitch.size(); ii++){
+        outFileMono << float(ii)*hopSize/sampleRate << ", " << pitchMono[ii] << endl;
+    }
+    string csvFilenameYin="/Users/GinSonic/MTG/EssentiaProject/Data/Monophonic/f0Yin/flute.csv";
+    ofstream outFileYin;
+    outFileYin.open(csvFilenameYin);
+    for (int ii=0; ii<pitch.size(); ii++){
+        outFileYin << float(ii)*hopSize/sampleRate << ", " << pitchYin[ii] << endl;
+    }
+    
+    
     // clean up
+    delete audio;
+    delete predmel;
+    delete predmelMono;
+    delete frameCutter;
+    delete pitchDetect;
+    delete window;
+    delete spectrum;
+    
     essentia::shutdown();
     
     return 0;
