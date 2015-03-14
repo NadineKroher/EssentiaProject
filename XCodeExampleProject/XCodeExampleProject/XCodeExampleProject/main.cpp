c6cc8523f4145e22b99f0f3843f161430ba6e126
nkroher <nadine.kroher@upf.edu>
2015-03-14 11:45:52 +0100

    XcodeExampleProject
    Initial project setup

---
+++ XCodeExampleProject/XCodeExampleProject/XCodeExampleProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  XCodeExampleProject
+//
+//  Created by Nadine Kroher on 14/03/15.
+//  Copyright (c) 2015 MTG. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
