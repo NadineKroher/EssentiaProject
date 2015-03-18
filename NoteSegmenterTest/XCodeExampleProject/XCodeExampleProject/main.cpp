16c5704c12ee3b7339495b6d2ed98c6e7da8c433
nkroher <nadine.kroher@upf.edu>
2015-03-18 17:23:02 +0100

    NoteSegmentationTest
    Test script for the essentia note segmentation algorithm.

---
+++ NoteSegmenterTest/XCodeExampleProject/XCodeExampleProject/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  XCodeExampleProject
+//
+//  Created by Nadine Kroher on 14/03/15.
+//  Copyright (c) 2015 MTG. All rights reserved.
+//
+
+#include <iostream>
+
+#include "essentia.h"
+#include "taglib.h"
+#include "fftw3.h"
+#include <essentia/algorithmfactory.h>
+#include <essentia/essentiamath.h>
+#include <essentia/pool.h>
+
+using namespace essentia;
+using namespace std;
+int main(int argc, const char * argv[]) {
+    string audioFilename="/Users/GinSonic/MTG/EssentiaProject/Data/monophonicExamples/test.wav";
+    return 0;
+}
