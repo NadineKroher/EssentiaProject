12481c9325f0af06d31400ec1215608f750283ac
nkroher <nadine.kroher@upf.edu>
2015-05-04 16:28:25 +0200

    multPitch implementation
    spurious peak likelihood

--- multiPitch/XCode implementation/XCodeExampleProject/main.cpp
+++ multiPitch/XCode implementation/XCodeExampleProject/main.cpp
@@ -147,19 +147,81 @@ int main(int argc, const char * argv[]) {
         for (int ii=0; ii<peakBins.size(); ii++){
             int bin=round(zpf*frameLen*peakBins[ii]/44100); // get relative magnitude
             float rMag=peakMags[ii]-fSpectrum[bin];
-            if (rMag>5 && peakBins[ii]>50 && peakBins[ii]<5000){ // select only peaks above the local threshold
-                peakBinsP.push_back(peakBins[ii]);
+            float mPeak=69+12*log2(peakBins[ii]/440); // conversion to MIDI number
+            if (rMag>5 && mPeak>midiMin){ // select only peaks above the local threshold and above min. considered F0
+                peakBinsP.push_back(mPeak);
                 peakMagsP.push_back(peakMags[ii]);
                 relMagsP.push_back(rMag);
             }
         }
 
-        cout << peakBinsP << endl;
         PeakData.push_back(peakBinsP);
         PeakAmpData.push_back(peakMagsP);
         RelPeakAmpData.push_back(relMagsP);
     }
     
     
+    
+    /////////////////////////////////////////////////////////
+    // STAGE 3-4: F0 candidate selection and f0 estimation //
+    /////////////////////////////////////////////////////////
+    vector<vector<float> > EstAllF0, EstAllLogLike;
+    
+    // spurious peak frequency and amplitude mean
+    vector<float> Spur_af_Miu;
+    Spur_af_Miu.push_back(23.0106);
+    Spur_af_Miu.push_back(82.0925);
+    
+    // spurious peak frequency and amplitude covariance
+    vector<vector<float> > Spur_af_Sigma;
+    vector<float> s0, s1;
+    s0.push_back(86.7795);
+    s0.push_back(-89.5364);
+    s1.push_back(-89.5364);
+    s1.push_back(481.6379);
+    Spur_af_Sigma.push_back(s0);
+    Spur_af_Sigma.push_back(s1);
+    float det=s0[0]*s1[1]-s0[1]*s1[0]; // determant
+    vector<float> inv0, inv1; // inverse
+    vector<vector<float> >inv;
+    inv0.push_back(s1[1]/det);
+    inv0.push_back(-s0[1]/det);
+    inv1.push_back(-s1[0]/det);
+    inv1.push_back(s0[0]/det);
+    inv.push_back(inv0);
+    inv.push_back(inv1);
+    
+    for (int i=0; i<PeakData.size(); i++){
+        
+        vector<float> FrAllF0, FrLogLike;
+        if (!PeakData[i].size()){ // no peaks found in this frame
+            EstAllF0.push_back(FrAllF0);
+            EstAllLogLike.push_back(FrLogLike);
+            continue;
+        }
+        
+        vector<float> p_SpurP;
+        for (int ii=0; ii<PeakData[i].size(); ii++){
+            
+            // spurious peak likelihood (single Gaussian model)
+            float x1=PeakData[i][ii];
+            float x2=PeakAmpData[i][ii];
+            float m1=Spur_af_Miu[0];
+            float m2=Spur_af_Miu[1];
+            float inv11=inv0[0];
+            float inv12=inv0[1];
+            float inv21=inv1[0];
+            float inv22=inv1[1];
+            float exponent=((x1-m1)*inv11+(x2-m2)*inv21)*(x1-m1)+((x1-m1)*inv12+(x2-m2)*inv22)*(x2-m2);
+            exponent*=0.5;
+            float nom=1/sqrt((det*2*3.14)*(det*2*3.14));
+            float val=nom*exp(exponent);
+            p_SpurP.push_back(val);
+            
+            int peakNum=PeakData[i].size();
+            int snum=int(peakNum/3);
+        }
+        
+    }
     return 0;
 }
