a5a224814f03c9ed8cadaf458656073ad706991d
nkroher <nadine.kroher@upf.edu>
2015-05-10 19:19:25 +0200

    multi pitch testing
    testing Klapuri multi pitch implementation

---
+++ KlapuriMultiPitch/EssentiaMelodiaVsVamp/main.cpp
@@ -0,0 +1,63 @@
+//////////////////////////////////////////////////////////////////////
+//  Extracts the pitch using the monophonic melodia implementation. //
+//////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <fstream>
+#include "essentia.h"
+#include "taglib.h"
+#include "fftw3.h"
+#include "avcodec.h"
+#include <essentia/algorithmfactory.h>
+#include <essentia/essentiamath.h>
+#include <essentia/pool.h>
+
+using namespace std;
+using namespace essentia;
+using namespace essentia::standard;
+
+int main(int argc, const char * argv[]) {
+    
+    // file path
+    string audioFilename="/Users/GinSonic/MTG/EssentiaProject/MelodiaMultiPitch/EssentiaMelodiaVsVamp/test2.wav";
+    
+    // parameters
+    int sampleRate = 44100;
+    int frameSize = 2048;
+    int hopSize = 128;
+    
+    // algorithm setup
+    essentia::init();
+    AlgorithmFactory& factory = standard::AlgorithmFactory::instance();
+    
+    Algorithm* audio = factory.create("MonoLoader","filename", audioFilename,"sampleRate", sampleRate);
+    Algorithm* multiPitch = factory.create("MultiPitch", "sampleRate", sampleRate);
+    vector<Real> audioVec;
+    vector<vector<Real> >pitchMulti;
+    
+    audio->output("audio").set(audioVec);
+    multiPitch->input("signal").set(audioVec);
+    multiPitch->output("pitch").set(pitchMulti);
+    
+    // Compute
+    audio->compute();
+    multiPitch->compute();
+    
+    // write to file
+    string csvFilenameMulti="/Users/GinSonic/MTG/EssentiaProject/MelodiaMultiPitch/EssentiaMelodiaVsVamp/test2.csv";
+    ofstream outFileMulti;
+    outFileMulti.open(csvFilenameMulti);
+    for (int ii=0; ii<pitchMulti.size(); ii++){
+        for (int jj=0; jj<pitchMulti[ii].size(); jj++){
+            outFileMulti <<  float(ii)*hopSize/sampleRate << ", " << pitchMulti[ii][jj] << endl;
+        }
+    }
+    
+    // clean up
+    delete audio;
+
+    
+    essentia::shutdown();
+    
+    return 0;
+}
